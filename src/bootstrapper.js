7426229bb0edf86be7e1750ade343ff4f7d205f9
zxvnme <igoranonim1337@gmail.com>
2018-01-15 22:23:15 +0100

    Bootstrapper and rank command changes

--- src/bootstrapper.js
+++ src/bootstrapper.js
@@ -1,18 +1,43 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const config = require('./../config.json');
-const fs = require('fs');
 const RiotApi = require('lol-stats-api-module');
+const config = require('./../config.json');
+const request = require('request');
+const Discord = require('discord.js');
+const chalk = require('chalk');
+const fs = require('fs');
+const client = new Discord.Client();
 const api = new RiotApi({
     key: config.apikey,
     region: config.region
 });
 
+
 client.on('ready', () => {
+    let success = chalk.green;
+    let error = chalk.red;
+    let neutral = chalk.gray;
+    let process_args = process.argv.slice(2);
+    switch (process_args[0]) {
+        case '--status':
+            switch (process_args[1]) {
+                case 'game':
+                    client.user.setGame(process_args[2]);
+                    console.log(success("Sucessfully ") + "set game status to " + neutral(process_args[2]));
+                    break;
+                case 'stream':
+                    client.user.setGame(process_args[2], "https://www.twitch.tv/wyvern_bot");
+                    console.log(success("Sucessfully ") + "set stream status to " + neutral(process_args[2]));
+                    break;
+            }
+            break;
+    }
+    api.getVersionsStaticData({ region: config.region }, (err, data) => {
+        if(!err) console.log(success("Estabilished ") + "connection with Riot Api v-" + neutral(data[0]));
+        else console.log(error("Couldn't ") + "connect to Riot Api. Error code: " + neutral(err.code));
+    });
     fs.readdir('./commands/', (err, files) => {
         files.forEach((response) => {
             if (!response.endsWith(".js")) return;
-            if (response) console.log("Loaded " + response);
+            if (response) console.log(success("Loaded ") + response);
         });
     });
 });
@@ -22,8 +47,8 @@ client.on('message', message => {
 
     if (!message.content.startsWith(config.prefix)) return;
 
-    const args = message.content.substring(config.prefix.length).split(" ");
-    const command = args[0].toLowerCase();
+    let args = message.content.substring(config.prefix.length).split(" ");
+    let command = args[0].toLowerCase();
 
     try {
         let commandFile = require('./commands/' + command + '.js');
