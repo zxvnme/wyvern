0b6ad9a3f594fa1b51d73224dcec3a2fabe1eef5
zxvnme <igoranonim1337@gmail.com>
2018-01-22 21:52:03 +0100

    Package.json update
    - Added license to package.json.
    - Some changes in author command.

--- src/commands/author.js
+++ src/commands/author.js
@@ -1,3 +1,5 @@
+const stringUtils = require('./../stringutils.js');
+
 module.exports.launch = (client, api, config, message, args) => {
     message.channel.send({
         embed: {
@@ -6,17 +8,13 @@ module.exports.launch = (client, api, config, message, args) => {
                 icon_url: "https://avatars.githubusercontent.com/zxvnme",
                 name: "zxvnme"
             },
+            description: stringUtils.wrapWithBold("Source code available at:") + "\n" +"https://github.com/zxvnme/wyvern" + "\n\n" + stringUtils.wrapWithBold("Official discord:") + "\n" + "https://discord.gg/3FVVyXW" + "\n",
             fields: [
                 {
-                    name: "Programmer:",
+                    name: "Programmer & Graphic designer:",
                     value: '[zxvnme](https://github.com/zxvnme)',
                     inline: true,
                 },
-                {
-                    name: "Graphic designer:",
-                    value: "[zxvnme](https://github.com/zxvnme)",
-                    inline: true,
-                }
             ],
             timestamp: new Date(),
             footer: {
